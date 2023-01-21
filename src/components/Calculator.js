429c250dd6132cbed9a9c4f3599c0656b19c5320
Outkarm <jkpordje@gmail.com>
2023-01-21 18:36:07 +0000

    change classes to hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,183 +1,182 @@
-import React from 'react';
-import calculate from './logic/calculate';
+import React, { useState } from "react";
+import calculate from "./logic/calculate";
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { total: 0, next: null, operation: null };
-  }
+const Calculator = () => {
+  const [display, newDisplay] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-  buttonClicked = (event) => {
+  const buttonClicked = (event) => {
     const btnName = event.target.innerText;
-    const answer = calculate(this.state, btnName);
-    this.setState(answer);
+    const answer = calculate(display, btnName);
+    newDisplay(answer);
   };
+  const { total, next, operation } = display;
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div id="calculator" className="calculator">
-        <div id="ans" className="ans">
-          {total}
-          {operation}
-          {next}
-        </div>
-        <div id="control-sec" className="control-sec">
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            AC
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            +/-
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            %
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn or"
-          >
-            ÷
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            7
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            8
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            9
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn or"
-          >
-            x
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            4
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            5
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            6
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn or"
-          >
-            -
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            1
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            2
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            3
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn or"
-          >
-            +
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn zero"
-          >
-            0
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn"
-          >
-            .
-          </button>
-          <button
-            type="button"
-            value="all-clear"
-            onClick={this.buttonClicked}
-            className="cBtn or"
-          >
-            =
-          </button>
-        </div>
+  return (
+    <div id="calculator" className="calculator">
+      <div id="ans" className="ans">
+        {total}
+        {operation}
+        {next}
       </div>
-    );
-  }
-}
+      <div id="control-sec" className="control-sec">
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          AC
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          +/-
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          %
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn or"
+        >
+          ÷
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          7
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          8
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          9
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn or"
+        >
+          x
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          4
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          5
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          6
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn or"
+        >
+          -
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          1
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          2
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          3
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn or"
+        >
+          +
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn zero"
+        >
+          0
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn"
+        >
+          .
+        </button>
+        <button
+          type="button"
+          value="all-clear"
+          onClick={buttonClicked}
+          className="cBtn or"
+        >
+          =
+        </button>
+      </div>
+    </div>
+  );
+};
 export { Calculator as default };
