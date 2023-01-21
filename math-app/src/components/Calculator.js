54eef27b61e77ec24019f333fc4c02c9f79097a4
Outkarm <jkpordje@gmail.com>
2023-01-21 11:04:07 +0000

    add basic calculator UI design

---
+++ math-app/src/components/Calculator.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div id="calculator" className="calculator">
+        <div id="ans" className="ans">
+          0
+        </div>
+        <div id="control-sec" className="control-sec">
+          <div className="cBtn">AC</div>
+          <div className="cBtn">+/-</div>
+          <div className="cBtn">%</div>
+          <div className="cBtn or">÷</div>
+          <div className="cBtn">7</div>
+          <div className="cBtn">8</div>
+          <div className="cBtn">9</div>
+          <div className="cBtn or">x</div>
+          <div className="cBtn">4</div>
+          <div className="cBtn">5</div>
+          <div className="cBtn">6</div>
+          <div className="cBtn or">-</div>
+          <div className="cBtn">1</div>
+          <div className="cBtn">2</div>
+          <div className="cBtn">3</div>
+          <div className="cBtn or">+</div>
+          <div className="cBtn zero">0</div>
+          <div className="cBtn">.</div>
+          <div className="cBtn or">=</div>
+        </div>
+      </div>
+    );
+  }
+}
+export { Calculator as default };
