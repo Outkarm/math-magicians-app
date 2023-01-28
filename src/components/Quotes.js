bcb43831d6a29b0a936b193cdefc1bf0f159569c
Outkarm <jkpordje@gmail.com>
2023-01-28 09:53:16 +0000

    add a full website ui

---
+++ src/components/Quotes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../style/quotes.css';
+
+function Quotes() {
+  return (
+    <div className="quotes-cont">
+      <h2 className="quotes-text">
+        {' '}
+        MatheMatics is not about numbers, equations, computations, or
+        alogorithams : it is about understanding. (KoyOutkarm).
+      </h2>
+    </div>
+  );
+}
+export { Quotes as default };
